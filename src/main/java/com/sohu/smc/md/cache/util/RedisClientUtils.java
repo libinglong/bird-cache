42c07d5cb863b3eb55b834fae1c773f01a5dfbf5
libinglong <libinglong9@gmail.com>
2020-12-02 20:39:59 +0800

    up

--- src/main/java/com/sohu/smc/md/cache/util/RedisClientUtils.java
+++ src/main/java/com/sohu/smc/md/cache/util/RedisClientUtils.java
@@ -3,7 +3,11 @@ package com.sohu.smc.md.cache.util;
 import io.lettuce.core.ClientOptions;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.TimeoutOptions;
 import io.lettuce.core.resource.ClientResources;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author binglongli217932
@@ -19,9 +23,14 @@ public class RedisClientUtils {
         } else {
             redisClient = RedisClient.create(clientResources, redisURI);
         }
+        TimeoutOptions timeoutOptions = TimeoutOptions.builder()
+                .connectionTimeout()
+                .fixedTimeout(Duration.of(3000, ChronoUnit.MILLIS))
+                .build();
         ClientOptions options = ClientOptions.builder()
                 .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
                 .cancelCommandsOnReconnectFailure(true)
+                .timeoutOptions(timeoutOptions)
                 .build();
         redisClient.setOptions(options);
         return redisClient;
